2adb50f064d8b344394440bfe0106446ce07e605
Washington-A <150960286+Washington-A@users.noreply.github.com>
2026-01-02 13:08:32 -0300

    feat: adiciona componente de scroll para o topo da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Router from "./routes/Router.tsx";
 import { HashRouter} from "react-router-dom";
 import {DeviceProvider} from "./contexts/DeviceContext.tsx";
+import {ScrollToTop} from "./routes/ScrollToTop.tsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
         <>
             <HashRouter>
                 <DeviceProvider>
+                    <ScrollToTop />
                     <Router/>
                 </DeviceProvider>
             </HashRouter>
